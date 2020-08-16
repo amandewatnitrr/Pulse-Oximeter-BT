6bacbcb47b6ac56853520b15e0338e5ded43d568
Aman Kumar Dewangan <52046981+amandewatnitrr@users.noreply.github.com>
2020-08-16 23:31:00 +0530

    Add files via upload
    Basic on how to read data from MAX30100 and repairing the sensor

---
+++ Pulse_Oximeter_test/Pulse_Oximeter_test.ino
@@ -0,0 +1,52 @@
+#include "MAX30100_PulseOximeter.h"
+ 
+#define REPORTING_PERIOD_MS     1000
+ 
+PulseOximeter pox;
+uint32_t tsLastReport = 0;
+ 
+void onBeatDetected()
+{
+    Serial.println("Beat!");
+}
+float sum; 
+void setup()
+{
+    
+    Serial.begin(115200);
+    Serial.print("Initializing pulse oximeter..");
+ 
+    // Initialize the PulseOximeter instance
+    // Failures are generally due to an improper I2C wiring, missing power supply
+    // or wrong target chip
+    if (!pox.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } else {
+        Serial.println("SUCCESS");
+    }
+     pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+ 
+    // Register a callback for the beat detection
+    pox.setOnBeatDetectedCallback(onBeatDetected);
+}
+ 
+void loop()
+{
+    // Make sure to call update as fast as possible
+    sum=0;
+    pox.update();
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+        Serial.print("Heart rate:");
+        for(int i=0;i<2000;i++)
+            {
+              sum=sum+pox.getHeartRate();
+            }
+        Serial.print(sum/2000);
+        Serial.print("bpm / SpO2:");
+        Serial.print(pox.getSpO2());
+        Serial.println("%");
+ 
+        tsLastReport = millis();
+    }
+}
